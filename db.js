256fd79b9dd46794e01aee4306b7fa6938c882d0
Neo Mashigo <neo.mashigo927@gmail.com>
2026-08-30 00:20:45 +0200

    Fix db.js and Yoco routes

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://ujconnect_user:bl7B4X24gNX1H6Lkwv3ZmVbG58Nla3cM@dpg-d85456jtqb8s73efogs0-a.virginia-postgres.render.com/ujconnect',
+  connectionString: process.env.DATABASE_URL || 'postgresql://smartclass_db_b8ch_user:Z6TpdL0pDKtywB3oHDzaJgFlZ3R1Mbaf@dpg-d9bcjkecjfls73ch3rm0-a.virginia-postgres.render.com/smartclass_db_b8ch',
   ssl: {
     rejectUnauthorized: false
   }
 });
 
 pool.on('connect', () => {
-  console.log('📦 SmartClass connected to UJconnect database');
+  console.log('📦 SmartClass connected to SMARTCLASS-DB');
 });
 
 pool.on('error', (err) => {
